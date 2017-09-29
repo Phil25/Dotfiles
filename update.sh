ace1520b4598c93fd8a94e0b3a4e4d4bdba69153
Phil25 <phil25@protonmail.com>
2017-09-29 20:00:51 +0200

    Updated laptop branch with xfce config

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ if [ $MODE == "out" -o $MODE == "o" ]; then
 	echo "Dotfiles exported to computer."
 else
 	cp ~/.{bash_profile,vimrc,xinitrc} .
-	cp -r ~/.config/{bumblebee-status,i3,rofi} ./.config/
+	cp -r ~/.config/{bumblebee-status,i3,rofi,xfce4} ./.config/
 	echo "Dotfiles imported to `pwd`."
 fi
 
