7ac734b3344ba852a9b5ac960c7c5e60d4cee525
Phil25 <phil25@protonmail.com>
2017-09-30 12:20:25 +0200

    Documented comp.sh

--- .vim/bundle/smcomp/comp.sh
+++ .vim/bundle/smcomp/comp.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# This script does the following:
+# 	- Compile .sp given as first parameter (absoulte path)
+#	- If successful, move .smx to ../plugins/
+#	- If second parameter is "c" or "custom":
+#		- Extract source to compile from the first parameter file
+#			- Information on external source must be on the first line
+#			- format: //$file.sp$ (relative paths work)
+#		- Compile and move if successful
+#
+# Source to compile must have spcomp in its directory!
+
 if [ $# -lt 1 ]; then
 	echo No file specified.
 	exit
@@ -22,11 +33,9 @@ _bin=${_file%.*}.smx
 
 cd $_path
 
-./spcomp "`pwd`/$_file"
-if ! [ -f $_bin ]; then
-	cd -
-	exit
+./spcomp "$_file"
+if [ -f $_bin ]; then
+	mv $_bin ../plugins/
 fi
-mv $_bin ../plugins/
 
 cd -
